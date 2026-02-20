e9f4176158a5a5f00c3face691c403cfd64e34b1
damarbagas-234 <muhammaddamar107@gmail.com>
2026-02-21 05:38:58 +0700

    improve CounterModel logic

--- lib/models/counter_model.dart
+++ lib/models/counter_model.dart
@@ -3,15 +3,26 @@ import 'package:flutter/foundation.dart';
 class CounterModel extends ChangeNotifier {
   int _angka = 0;
 
+  // Getter untuk mengambil nilai counter
   int get angka => _angka;
 
+  // Menambah nilai counter
   void increment() {
     _angka++;
     notifyListeners();
   }
 
+  // Mengurangi nilai counter (tidak boleh kurang dari 0)
   void decrement() {
-    _angka--;
+    if (_angka > 0) {
+      _angka--;
+      notifyListeners();
+    }
+  }
+
+  // Reset nilai counter ke 0
+  void reset() {
+    _angka = 0;
     notifyListeners();
   }
 }
