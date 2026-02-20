ce9f9e9d4abec1b7f0139c1471a8e6e1626cf58b
damarbagas-234 <muhammaddamar107@gmail.com>
2026-02-21 05:13:05 +0700

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: const MyHomePage(title: 'Damar Bagas'),
+        home: const MyHomePage(title: 'Damar Ganteng'),
         debugShowCheckedModeBanner: false,
       ),
     );
